5c00cdd06c3113f1b1e640e0da3955fdfae518fb
leorossi <rl.rossileonardo@gmail.com>
2017-06-21 15:34:06 +0200

    .

--- src/main/java/it/uniroma3/JsonUtils/Convertitore.java
+++ src/main/java/it/uniroma3/JsonUtils/Convertitore.java
@@ -31,45 +31,45 @@ public class Convertitore {
 		return jsonArray;
 	}
 
-//	/**
-//	 * Converte il Result della query Cypher in jsonArray
-//	 */
-//	public static JsonArray convertCypherToJSON(Result result) throws Exception {
-//		JsonArray jsonArray = new JsonArray();
-//		while ( result.hasNext() ) {
-//			Map<String, Object> row = result.next();
-//			System.out.println("\n\nROW = "+row.toString()+"\n\n");
-//			JsonObject obj = new JsonObject();
-//			for ( String key : result.columns()){
-//				System.out.println("KEY = "+key);
-//				System.out.println("row.get(key).toString() = "+ row.get(key).toString());
-//				obj.addProperty(key,row.get(key).toString());
-//			}
-//			jsonArray.add(obj);
-//
-//		}
-//		return jsonArray;
-//	}
-	
+	/**
+	 * Converte il Result della query Cypher in jsonArray
+	 */
 	public static JsonArray convertCypherToJSON(Result result) throws Exception {
 		JsonArray jsonArray = new JsonArray();
 		while ( result.hasNext() ) {
-
-
 			Map<String, Object> row = result.next();
+			System.out.println("\n\nROW = "+row.toString()+"\n\n");
 			JsonObject obj = new JsonObject();
-			StringBuilder ennupla = new StringBuilder();
-			for ( String key : result.columns() ) {
-				Iterator i = result.columnAs(key);
-				while (i.hasNext())	{
-					ennupla.append(i.toString());
-				}
-				obj.addProperty(key,ennupla.toString());
+			for ( String key : result.columns()){
+				System.out.println("KEY = "+key);
+				System.out.println("row.get(key).toString() = "+ row.get(key).toString());
+				obj.addProperty(key,row.get(key).toString());
 			}
 			jsonArray.add(obj);
+
 		}
 		return jsonArray;
 	}
+//	
+//	public static JsonArray convertCypherToJSON(Result result) throws Exception {
+//		JsonArray jsonArray = new JsonArray();
+//		while ( result.hasNext() ) {
+//
+//
+//			Map<String, Object> row = result.next();
+//			JsonObject obj = new JsonObject();
+//			StringBuilder ennupla = new StringBuilder();
+//			for ( String key : result.columns() ) {
+//				Iterator i = result.columnAs(key);
+//				while (i.hasNext())	{
+//					ennupla.append(i.toString());
+//				}
+//				obj.addProperty(key,ennupla.toString());
+//			}
+//			jsonArray.add(obj);
+//		}
+//		return jsonArray;
+//	}
 
 }
 
